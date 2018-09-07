89cd918b8e86fdd8f0dfa507037e6fdd2217cebc
Swetha <swetha@Swethas-MacBook-Pro.local>
2018-09-07 11:30:36 +0530

    changes to pagebuilder library

--- src/main/java/PageBuilder/Shipping/Aramex.java
+++ src/main/java/PageBuilder/Shipping/Aramex.java
@@ -1,9 +1,7 @@
 package PageBuilder.Shipping;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class Aramex {
 	WebDriver driver;
